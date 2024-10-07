65f0876505e43928ecc7505ffae636fdc3e59196
Aditya Nugrahahttps://github.com/account <adityanugraha1523@gmail.com>
2024-10-07 18:57:06 +0700

    Atur layout

--- views/gudang/DaftarGudang.php
+++ views/gudang/DaftarGudang.php
@@ -38,6 +38,7 @@ $gudangsForCurrentPage = array_slice($gudangs, $offset, $itemsPerPage); // Menga
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style.css"> <!-- Mengimpor file CSS khusus -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> <!-- Memuat font Poppins -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> <!-- Mengimpor Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet"> <!-- Mengimpor Bootstrap Icons -->
     <title>Daftar Gudang MSIB</title> <!-- Judul halaman -->
